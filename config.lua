def88d37c802f255288dfbb6f99918de9a6140d7
reylagardo <88545906+reylagardo@users.noreply.github.com>
2026-02-23 02:57:28 +0700

    config.lua

---
+++ config.lua
@@ -0,0 +1,26 @@
+Config = {}
+
+Config.LocalDistance = 20.0
+Config.LocalColorHex = '#00e5ff'
+Config.OOCColorHex = '#06f81f'
+
+Config.JobCommands = {
+    ['pol'] = { 
+        label = 'POLISI', 
+        jobName = 'police', -- Nama job di database
+        colorHex = '#1e90ff',
+        logo = 'https://pngimg.com/uploads/police_badge/police_badge_PNG85.png' 
+    },
+    ['med'] = { 
+        label = 'MEDIS', 
+        jobName = 'ambulance', -- Nama job di database
+        colorHex = '#ff4757',
+        logo = 'https://images.seeklogo.com/logo-png/35/2/medis-logo-png_seeklogo-356902.png' 
+    },
+    ['mech'] = { 
+        label = 'MEKANIK', 
+        jobName = 'mechanic', -- Nama job di database
+        colorHex = '#ffa502',
+        logo = 'https://png.pngtree.com/png-clipart/20250305/original/pngtree-cheerful-mechanic-logo-friendly-and-approachable-branding-png-image_20576927.png' 
+    }
+}
