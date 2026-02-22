96a5a320cdd433d548f8a02aa5ca490cc731ecc4
reylagardo <88545906+reylagardo@users.noreply.github.com>
2026-02-23 02:56:53 +0700

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_scripts {
+    'config.lua'
+}
+
+ui_page 'my-chat-fivem/dist/index.html'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+files {
+    'my-chat-fivem/dist/index.html',
+    'my-chat-fivem/dist/assets/*.js',
+    'my-chat-fivem/dist/assets/*.css'
+}
